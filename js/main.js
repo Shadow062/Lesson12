b65bbef6412dc266bd5f0b624ffe49c84d2b5f07
Ali <a.dadawov@outlook.com>
2020-04-23 20:48:22 +0300

    StopTime

--- js/main.js
+++ js/main.js
@@ -35,4 +35,56 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+    // Пишем таймер
+    let deadline = '2020-04-25 00:00';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+        // hours = Math.floor((t / 1000 / 60 / 60) % 24),
+        // Что бы получить количество дней
+        // days = Math.floor((t / (1000 * 60 * 60 * 24)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    function zero(a) {
+        if (a < 10) {
+            a = '0' + a;
+        }
+        return a;
+    }
+
+    setClock('timer', deadline);
 });
